b649945d6a5e979ce9f769d5d61ea6937d4a984d
Diego <diego.quispe.qui@tecsup.edu.pe>
2025-04-08 14:23:46 -0500

    Adicion del LazyColumn

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.prueba.ui.theme.PruebaTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +31,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun LazyColumnExample() {
+    LazyColumn {
+        items(100) { index ->
+            Text(text = "Item #$index", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
