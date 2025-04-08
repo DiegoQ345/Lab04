488f00797b37f2ce87e5c398c95cec811e78cf4a
Diego <diego.quispe.qui@tecsup.edu.pe>
2025-04-08 14:24:37 -0500

    Adicion del ShowAlertDialog

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +42,24 @@ fun LazyColumnExample() {
         items(100) { index ->
             Text(text = "Item #$index", modifier = Modifier.padding(16.dp))
         }
+    }
+}
+
+
+@Composable
+fun ShowAlertDialog() {
+    val openDialog = remember { mutableStateOf(false) }
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = { openDialog.value = false },
+            title = { Text("Alerta") },
+            text = { Text("Este es un mensaje de alerta.") },
+            confirmButton = {
+                TextButton(onClick = { openDialog.value = false }) {
+                    Text("Aceptar")
+                }
+            }
+        )
     }
 }
 
